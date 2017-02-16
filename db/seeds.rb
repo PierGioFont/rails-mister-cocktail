5eeb63c9400d363fdfa9c4398af3668e8efb81ab
PierGiovanni Fontana <piergiovanni@cegetel.net>
2017-02-16 14:43:01 +0100

    first controller ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+
+filepath = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+file = open(filepath).read
+ingredients = JSON.parse(file)
+ingredients.each_with_index do |ingredient, ind|
+  ingredient[1].each do |ing|
+    Ingredient.create(name: ing["strIngredient1"])
+  end
+end
